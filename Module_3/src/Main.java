0ae4474979ec6509eea8e218db2579583c371015
OlegAlex <olegalexod@gmail.com>
2017-09-25 22:21:22 +0300

    Обновленный проект

--- Module_3/src/Main.java
+++ Module_3/src/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 
 public class Main {
+
+
+
+     /*bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+    }*/
     public static void main( String [] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
@@ -44,6 +58,9 @@ public class Main {
             }
         }
         System.out.println("Количество повторений числа 5: " + j);
+
+
+
     }
 }
 
