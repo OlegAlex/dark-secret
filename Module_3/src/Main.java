7cbb145f40b6af0dd2c4a7c961b55d266c3ab507
OlegAlex <olegalexod@gmail.com>
2017-09-25 22:16:18 +0300

    Третий модуль

---
+++ Module_3/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main( String [] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = input.nextInt();
+        int num[] = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            num[i] = input.nextInt();
+        }
+        System.out.print("Введенные элементы массива:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + num[i]);
+        }
+        System.out.println();
+
+
+
+        //Поиск максимального значения из массива
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+            max = Math.max(max, num[i]);
+        }
+        System.out.println("Максимальный элемент в массиве num: " + max);
+
+
+        //Поиск минимального значения массива
+        int min = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] < min) {
+                min = num[i];
+            }
+        }
+        System.out.println("Минимальное значение в массиве num: " + min);
+
+        //Поиск количества повторений числа 5
+        int j = 0;
+        for(int i=0; i<size; i++) {
+            if (num[i]== 5){
+                j++;
+            }
+        }
+        System.out.println("Количество повторений числа 5: " + j);
+    }
+}
+
+
+
+
+
+
+
+
